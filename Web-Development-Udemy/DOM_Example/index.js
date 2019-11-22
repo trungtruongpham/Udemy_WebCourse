a229468e50f9ab38dc968b7185c904a9a019e7ad
trungtruongpham <trungtruongpham@gmail.com>
2019-11-22 23:41:58 +0700

    Setup Folder

---
+++ Web-Development-Udemy/DOM_Example/index.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("li")[1].style.color = "red";
+document.getElementsByTagName("li").length;
+
+
+document.querySelector("li a");
+document.querySelector("button").style.backgroundColor = "red";
+
+document.querySelector("button").classList.add("visible");
+document.querySelector("h1").classList.add("huge");
--- Web-Development-Udemy/Udemy-Web/DOM_Example/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByTagName("li")[1].style.color = "red";
-document.getElementsByTagName("li").length;
-
-
-document.querySelector("li a");
-document.querySelector("button").style.backgroundColor = "red";
-
-document.querySelector("button").classList.add("visible");
-document.querySelector("h1").classList.add("huge");
